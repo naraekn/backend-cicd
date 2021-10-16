974913e31c7f998ce51d39e3456a86c982493820
nankim <nan.kim@kurlycorp.com>
2021-10-16 21:56:05 +0900

    Add continous deployment to github actions

--- app/src/main/java/com/naraekn/cicd/controllers/HealthCheckController.java
+++ app/src/main/java/com/naraekn/cicd/controllers/HealthCheckController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping
     String sayHello() {
-        return "Health Check";
+        return "Health Check - deployment";
     }
 }
